9730ba39ba269b034fb93fb3176e7152e9a7faa4
lizhu129 <lizhu129@gmail.com>
2022-06-19 14:32:30 +0200

    Initial commit

---
+++ react-frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ListStudentComponent from "./Components/ListStudentComponent";
+
+function App() {
+  return (
+    <div className="container">
+      <ListStudentComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-frontend/src/Components/ListStudentComponent.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+class ListStudentComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            students: []
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h2 className="text-center">Students List</h2>
+                <div className="row">
+                    <table className="table table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Email Address</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.students.map(
+                                    student =>
+                                    <tr key = {student.Id}>
+                                        <td> {student.firstName} </td>
+                                        <td> {student.lastName} </td>
+                                        <td> {student.email} </td>                                 
+                                    </tr>
+                                )
+                            }
+                        </tbody>
+
+                    </table>
+                </div>
+                
+            </div>
+        );
+    }
+}
+
+export default ListStudentComponent;
