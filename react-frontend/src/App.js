36e63d6f6fe759d0f62fcd0d4d7e7b43ab226776
lizhu129 <lizhu129@gmail.com>
2022-06-24 20:41:12 +0200

    configure routing

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import ListStudentComponent from "./components/ListStudentComponent";
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
@@ -7,11 +9,18 @@ import FooterComponent from './components/FooterComponent';
 function App() {
   return (
     <div>
-      <HeaderComponent />       
-          <div className="container">
-            <ListStudentComponent />
-          </div>
-          <FooterComponent />
+      <Router>
+        <div className='container'>
+          <HeaderComponent />       
+            <div className="container">
+              <Routes>
+                <Route path='/' element={<ListStudentComponent />}></Route>
+                <Route path='/students' element={<ListStudentComponent />}></Route>
+              </Routes>
+            </div>
+              <FooterComponent />
+            </div>
+      </Router>   
     </div>
   );
 }
